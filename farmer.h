62d411e64367ef1d425d50dee34c570fecce274f
Erfan Ahmed Sanim <erfanahmedsanim@gmail.com>
2025-12-02 00:03:43 +0600

    Update farmer.h

--- farmer.h
+++ farmer.h
@@ -1,12 +1,45 @@
 #ifndef FARMER_H
 #define FARMER_H
 
-#include "product.h"
+#define PRODUCT_FILE "products.txt"
+#define ORDER_FILE   "orders.txt"
 
-void addProduct();
-void viewMyProducts();
-void updateProduct();
-void deleteProduct();
-void viewOrders();
+
+
+typedef struct {
+    char name[50];
+    char phone[20];
+    char password[20];
+} Farmer;
+
+typedef struct {
+    int id;
+    char name[50];
+    float price;
+    int quantity;
+    char farmerPhone[20];
+} Product;
+
+typedef struct {
+    int orderID;
+    int productID;
+    char productName[50];
+    int quantity;
+    char consumerPhone[20];
+    char farmerPhone[20];
+} Order;
+
+
+
+void clearInputBuffer();
+void removeNewLine(char *str);
+int getNewProductID();
+
+
+void addProduct(char farmerPhone[]);
+void viewMyProducts(char farmerPhone[]);
+void updateProduct(char farmerPhone[]);
+void deleteProduct(char farmerPhone[]);
+void viewOrders(char farmerPhone[]);
 
 #endif
